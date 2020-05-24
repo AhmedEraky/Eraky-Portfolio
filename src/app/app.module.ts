7b28a38264435c113bd692dece0707611c28916b
AhmedEraky <ahmedmoeraky@gmail.com>
2020-05-24 07:43:52 +0200

    add Nav Bar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import {CoreModule} from './core/core.module';
+import { NavComponent } from './layout/nav/nav.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    NgbModule.forRoot()
+    CoreModule
   ],
   providers: [],
   bootstrap: [AppComponent]
